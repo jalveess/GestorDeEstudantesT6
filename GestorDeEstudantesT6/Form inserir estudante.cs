e4ac3d21925692cce17e41f12f518cdd98cb5682
jalveess <167465724+jalveess@users.noreply.github.com>
2024-05-28 15:23:06 -0300

    Enviei o projeto.
    Enviei o projeto.

---
+++ GestorDeEstudantesT6/Form inserir estudante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorDeEstudantesT6
+{
+    public partial class Form_inserir_estudante : Form
+    {
+        public Form_inserir_estudante()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_inserir_estudante_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
